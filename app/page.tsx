f288b63bf2824e5e1212fadb805c08b72f393fb4
aarushk09 <aarushkute8@gmail.com>
2026-02-24 00:40:49 -0500

    test 5 linux pipeline

--- app/page.tsx
+++ app/page.tsx
@@ -216,7 +216,7 @@ export default function Portfolio() {
                     <p className="font-crimson-text text-xl md:text-2xl text-zinc-300 leading-relaxed max-w-3xl">
                       I'm passionate about artificial intelligence and machine learning, with a keen interest in
                       Arduino projects. In my free time, I enjoy playing the piano and tennis. My career goal is to work
-                      in the AI/L field while pursuing entrepreneurial ventures.
+                      in the AI/MML field while pursuing entrepreneurial ventures.
                     </p>
                   </div>
 
